34f4ef8f03703736636e309390446cebba162b59
Memory Clutter <memclutter@gmail.com>
2015-09-10 12:25:27 +0300

    Add two city, prepare to run on device

--- www/js/services.js
+++ www/js/services.js
@@ -5,6 +5,16 @@ angular.module('starter.services', [])
         name: 'Москва',
         desc: 'Столица нашей Родины',
         emblem: 'http://upload.wikimedia.org/wikipedia/commons/d/da/Coat_of_Arms_of_Moscow.png'
+      }, {
+        id: 542420,
+        name: 'Краснодар',
+        desc: 'Южная столица Родины',
+        emblem: 'https://upload.wikimedia.org/wikipedia/commons/8/80/Coat_of_Arms_of_Krasnodar_%28Krasnodar_krai%29.png'
+      }, {
+        id: 540761,
+        name: 'Кропоткин',
+        desc: 'Моя родина',
+        emblem: 'https://upload.wikimedia.org/wikipedia/commons/4/48/Coat_of_Arms_of_Kropotkin_%28Krasnodar_krai%29.png'
       }];
       return {
         all: function() {
